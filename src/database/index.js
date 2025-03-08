fa346419098841090cf75150e8b9fe7eaf2485f6
Nirajan09 <nirajantiwari09@gmail.com>
2025-03-08 15:14:23 +0545

    database

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const { default: mongoose } = require("mongoose");
 
 const connectToDB=async()=>{
-    const connectionURL="mongodb+srv://eeeeeeeerohit:IxgNX7iioKkblInS@cluster0.63464.mongodb.net/";
+    const connectionURL=process.env.MONGODB_URI
     mongoose.connect(connectionURL).then(()=>console.log("Connection is Successful")).catch((e)=>console.log("Something went wrong"));
 }
 export default connectToDB
